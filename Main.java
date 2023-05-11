1b192268ca590860f4dd1bd3af5cfd4986a57285
drakoshllk <lkaratel@list.ru>
2023-05-11 20:08:01 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        MyBinaryTreeMap<Integer> test = new MyBinaryTreeMap<>();
+        test.put(100);
+        test.put(80);
+        test.put(135);
+        test.put(40);
+        test.put(90);
+        test.put(120);
+        test.put(170);
+        test.put(20);
+        test.put(60);
+        test.put(10);
+        test.put(50);
+        test.put(70);
+        test.put(55);
+        test.printTree();
+        
+
+    }
+}
